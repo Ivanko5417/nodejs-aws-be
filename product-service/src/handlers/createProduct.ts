5fb8f00268d96caa00a0ad57f69d4e7349d3258d
Ivanko5417 <v.drobysh2011@gmail.com>
2020-11-07 15:58:15 +0300

    refactor: add logging request data

--- product-service/src/handlers/createProduct.ts
+++ product-service/src/handlers/createProduct.ts
@@ -3,8 +3,11 @@ import 'source-map-support/register';
 import { Product } from '../types';
 import * as productService from '../services/product/product.service';
 import { processError, processResponse } from '../utils/response.helper';
+import { logRequest } from '../utils/logging';
 
 export default async (event: APIGatewayProxyEventBase<APIGatewayEventDefaultAuthorizerContext>) => {
+  logRequest(event);
+
   try {
     const productToCreate: Product = JSON.parse(event.body);
 
--- product-service/src/handlers/getProductById.ts
+++ product-service/src/handlers/getProductById.ts
@@ -2,8 +2,11 @@ import { APIGatewayEventDefaultAuthorizerContext, APIGatewayProxyEventBase } fro
 import 'source-map-support/register';
 import * as productService from '../services/product/product.service';
 import { processError, processResponse } from '../utils/response.helper';
+import { logRequest } from '../utils/logging';
 
 export default async (event: APIGatewayProxyEventBase<APIGatewayEventDefaultAuthorizerContext>) => {
+  logRequest(event);
+
   try {
     const { id } = event.pathParameters;
 
