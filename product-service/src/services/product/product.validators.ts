aae1fd016cb6f91f412677f6e10e3be19e80eebc
Ivanko5417 <v.drobysh2011@gmail.com>
2020-11-10 17:52:25 +0300

    fix: allow only integer value for price and count

--- product-service/src/services/product/product.validators.ts
+++ product-service/src/services/product/product.validators.ts
@@ -5,8 +5,8 @@ import { ValidationError } from '../../exceptions';
 const productSchema = Joi.object().keys({
   title: Joi.string().required(),
   description: Joi.string().required(),
-  price: Joi.number().min(0).required(),
-  count: Joi.number().min(1).required()
+  price: Joi.number().integer().min(0).required(),
+  count: Joi.number().integer().min(1).required()
 });
 
 export function validateProduct(product: Product) {
